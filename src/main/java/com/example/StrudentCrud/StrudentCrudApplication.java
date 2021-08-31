7fe01c8e43f429f5e66ab9a47ceb234b62dfe9ee
Aditya Dixit <dixitaditya20@gmail.com>
2021-08-31 16:56:34 +0530

    my Crud Project

---
+++ src/main/java/com/example/StrudentCrud/StrudentCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.StrudentCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrudentCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrudentCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StrudentCrud/StrudentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StrudentCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrudentCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
